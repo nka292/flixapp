609800920a377acf6a78e775798d3e938b5fe6ec
Nana Asubonteng <namankwah16@students.desu.edu@Nanas-MacBook-Pro.local>
2019-02-05 19:35:04 -0800

    First Commit

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by Nana Asubonteng on 2/5/19.
+//  Copyright © 2019 Nana Asubonteng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
